39e2d23231d352211b0b670731785c777b5bd36a
fitz95 <fitzgeraldnsong@gmail.com>
2023-05-31 08:54:27 +0100

    initial setup

---
+++ app/views/entities/_entity.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! entity, :id, :name, :amount, :AuthorID, :created_at, :updated_at
+json.url entity_url(entity, format: :json)
---
+++ db/migrate/20230529191506_create_entities.rb
@@ -0,0 +1,11 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.integer :amount
+      t.string :AuthorID
+
+      t.timestamps
+    end
+  end
+end
